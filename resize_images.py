69fa25867e30f21a1493529baa89b7a688a4fa9b
sripaad <sripaad751@gmail.com>
2020-08-28 04:00:39 +0530

    ned to add parallel

---
+++ resize_images.py
@@ -0,0 +1,31 @@
+import os
+import argparse
+import re
+import glob
+from tqdm import tqdm
+from PIL import Image, ImageFile
+from joblib import Parallel, delayed
+import resizer
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+def resize_images():
+    
+    #taking user arguments
+    parser = argparse.ArgumentParser(description="Resize images")
+    parser.add_argument(
+        "-i", "--inputdir", default=".", type=str, help="image path input directory"
+    )
+    parser.add_argument(
+        "-o", "--outputdir", default=".", type=str, help="output save directory"
+    )
+    parser.add_argument(
+        "-r", "resize", default="512x512", type=str, help="image resize dimentions"
+    )
+    args = parser.parse_args()
+    input_folder = args.inputdir
+    output_folder = args.outputdir
+    resize = args.resize
+    resize = re.split(r"x|X", resize)
+   
